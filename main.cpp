39a2aada1a1c953b7c665897f62583a4aea82414
Lucas Gutierrez <lucasgutie@Martinas-MacBook-Pro.local>
2025-05-28 17:45:40 -0300

    Intento de Compilación USB

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include "mbed.h"
+#include "USBMIDI.h"
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE_MS     50
+#define BLINKING_RATE_MS     500
+USBMIDI midi;
 
 int main()
 {
     // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
-
+    DigitalOut led(PC_13);
     while (true) {
         led = !led;           
-        printf("Blinky\r\n");
+        /*
+        for(int i=48; i<83; i++) {     // send some messages!
+            midi.write(MIDIMessage::NoteOn(i));
+            ThisThread::sleep_for(50ms);
+            midi.write(MIDIMessage::NoteOff(i));
+            ThisThread::sleep_for(50ms);
+        }*/
         ThisThread::sleep_for(BLINKING_RATE_MS);
     }
 }
